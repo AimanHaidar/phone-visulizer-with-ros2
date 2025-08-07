89ecaae4cc18f27247d4d5f10ee981f71b48eb65
AimanHaidar <atahalabsi@gmail.com>
2025-08-07 20:58:34 +0300

    add launch file to launch phonesenor with rviz at once

---
+++ src/vis_phone/launch/display.launch.py
@@ -0,0 +1,32 @@
+from launch import LaunchDescription
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.actions import Node
+from launch.substitutions import Command
+import os
+from ament_index_python.packages import get_package_share_path
+
+def generate_launch_description():
+
+    urdf_path = os.path.join(get_package_share_path('vis_phone'),
+                             'urdf', 'phone.urdf.xacro')
+    rviz_config_path = os.path.join(get_package_share_path('vis_phone'),
+                                    'rviz', 'my_phone.rviz')
+    
+    robot_description = ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
+
+    robot_state_publisher_node = Node(
+        package="phone_orientation",
+        executable="phonesensor_node",
+        #parameters=[{'robot_description': robot_description}]
+    )
+
+    rviz2_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=['-d', rviz_config_path]
+    )
+
+    return LaunchDescription([
+        robot_state_publisher_node,
+        rviz2_node
+    ])

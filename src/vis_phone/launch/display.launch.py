a509fa578e61549c5a78bb257f817c9430c4ca5d
AimanHaidar <atahalabsi@gmail.com>
2025-10-22 16:34:56 +0300

    add show rviz launch argument

--- src/vis_phone/launch/display.launch.py
+++ src/vis_phone/launch/display.launch.py
@@ -1,22 +1,33 @@
 from launch import LaunchDescription
-from launch_ros.parameter_descriptions import ParameterValue
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.actions import Node
-from launch.substitutions import Command
+from launch_ros.parameter_descriptions import ParameterValue
 import os
-from ament_index_python.packages import get_package_share_path
-from ament_index_python.packages import get_package_share_directory
+from ament_index_python.packages import get_package_share_path, get_package_share_directory
+
 
 def generate_launch_description():
 
+    show_rviz_arg = DeclareLaunchArgument(
+        'show_rviz',
+        default_value='true',   # must be string, not bool!
+        description='Whether to show RViz'
+    )
+
+    show_rviz = LaunchConfiguration('show_rviz')
+
     urdf_path = os.path.join(get_package_share_path('vis_phone'),
                              'urdf', 'phone.urdf.xacro')
+
     rviz_config_path = os.path.join(get_package_share_path('vis_phone'),
                                     'rviz', 'my_phone.rviz')
     
     phone_orientation_params = os.path.join(
         get_package_share_directory("vis_phone"), 
         "config", "phone_orient_params.yaml")
-    
+
     robot_description = ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
 
     phone_orientation_node = Node(
@@ -37,7 +48,8 @@ def generate_launch_description():
             ('/tf_static','/phone/tf_static'),
             ('/joint_state','/phone/joint_state'),
         ],
-        output="screen"
+        output="screen",
+        condition=IfCondition(show_rviz)
     )
 
     robot_state_publisher_node = Node(
@@ -50,11 +62,14 @@ def generate_launch_description():
             ('/tf_static','/phone/tf_static'),
             ('/joint_state','/phone/joint_state'),
         ],
+        condition=IfCondition(show_rviz)
     )
 
-
-    return LaunchDescription([
+    ld = LaunchDescription([
+        show_rviz_arg,
         robot_state_publisher_node,
-        phone_orientation_node,
-        rviz2_node
+        rviz2_node,
+        phone_orientation_node
     ])
+
+    return ld
